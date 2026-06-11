0776dbb7d77fa18a97afa0a290a85286daf5e742
Chiara <chiaralinacasadei@gmail.com>
2026-06-10 21:05:42 -0300

    add remaining files

---
+++ src/main/java/tuti/desi/NameNotFoundApplication.java
@@ -0,0 +1,13 @@
+package tuti.desi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameNotFoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameNotFoundApplication.class, args);
+	}
+
+}
